2602a175b05fb7887e6072d3f77a2cdacbdb2148
chrismcdonnell <chris.mcdonnell@tstllc.net>
2023-11-05 21:48:20 -0500

    Restored normal on_attach behavior

--- lua/ecinori/lspconfig.lua
+++ lua/ecinori/lspconfig.lua
@@ -3,19 +3,19 @@ local nnoremap = require("ecinori.keymap").nnoremap
 local capabilities = require 'cmp_nvim_lsp'.default_capabilities()
 
 
--- local on_attach = function(client, bufnr)
---     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
---
---     local bufopts = { noremap = true, silent = true, buffer = bufnr }
---
---     -- nnoremap('gD', vim.lsp.buf.declaration, bufopts)
---     -- nnoremap('gd', vim.lsp.buf.definition, bufopts)
---     -- nnoremap('gr', vim.lsp.buf.references, bufopts)
---     -- nnoremap('gi', vim.lsp.buf.implementation, bufopts)
---     -- nnoremap('<leader>rn', vim.lsp.buf.rename, bufopts)
---     -- nnoremap('K', vim.lsp.buf.hover, bufopts)
---     -- nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts)
--- end
+local on_attach = function(client, bufnr)
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+    -- nnoremap('gD', vim.lsp.buf.declaration, bufopts)
+    -- nnoremap('gd', vim.lsp.buf.definition, bufopts)
+    -- nnoremap('gr', vim.lsp.buf.references, bufopts)
+    -- nnoremap('gi', vim.lsp.buf.implementation, bufopts)
+    -- nnoremap('<leader>rn', vim.lsp.buf.rename, bufopts)
+    -- nnoremap('K', vim.lsp.buf.hover, bufopts)
+    -- nnoremap('<C-k>', vim.lsp.buf.signature_help, bufopts)
+end
 --
 local lsp_flags = {
     debounce_text_changes = 150,
