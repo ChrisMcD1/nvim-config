afd710b922ce31e83dbd30770f7a6ac04cdecdb4
Chris McDonnell <camcdonn@gmail.com>
2023-12-05 21:44:51 -0500

    Upgraded telescope tag to fix bug

--- lua/ecinori/packer.lua
+++ lua/ecinori/packer.lua
@@ -53,7 +53,6 @@ require('packer').startup(function()
     }
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
         "kylechui/nvim-surround",
         tag = "*",
         config = function()
@@ -62,6 +61,7 @@ require('packer').startup(function()
     }
 
     use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
 
