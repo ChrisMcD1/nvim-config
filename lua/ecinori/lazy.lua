b10d49e81bbe6e3980be6133d38ea733be9728eb
Chris McDonnell <camcdonn@gmail.com>
2023-12-12 00:13:43 -0500

    Set up nvim-tmux-navigation

--- lua/ecinori/lazy.lua
+++ lua/ecinori/lazy.lua
@@ -26,6 +26,19 @@ require('lazy').setup({
             'nvim-lua/plenary.nvim'
         }
     },
+    {
+        'alexghergh/nvim-tmux-navigation',
+        config = function()
+            local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+            vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+            vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+            vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+            vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+            vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+            -- nnoremap("<C-Space> <Cmd>NvimTmuxNavigateNext<CR>
+        end
+    },
 
     {
         'numToStr/Comment.nvim',
