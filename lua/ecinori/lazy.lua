25bb276611385a4ca745ad295af57f9ee111c354
Chris McDonnell <camcdonn@gmail.com>
2023-12-13 23:28:06 -0500

    Added harpoon

--- lua/ecinori/lazy.lua
+++ lua/ecinori/lazy.lua
@@ -104,5 +104,10 @@ require('lazy').setup({
         tag = '0.1.4',
         dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
+    {
+        'ThePrimeagen/harpoon',
+        branch = "harpoon2",
+        dependencies = { { 'nvim-lua/plenary.nvim' } }
+    }
 
 })
