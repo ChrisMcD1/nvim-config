24e5ed4e5bdc0b8c9311bdef78ee508382d19f59
ChrisMcD1 <c.a.mcdonn@gmail.com>
2024-01-05 15:36:08 -0500

    Made metals actually respect the metals_config :D

--- lua/ecinori/lspconfig.lua
+++ lua/ecinori/lspconfig.lua
@@ -60,7 +60,7 @@ local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = t
 vim.api.nvim_create_autocmd("FileType", {
     pattern = { "scala", "sbt", "java" },
     callback = function()
-        require("metals").initialize_or_attach({})
+        require("metals").initialize_or_attach(metals_config)
     end,
     group = nvim_metals_group,
 })
