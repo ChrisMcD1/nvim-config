8107acf6b3209785cc34417843eb9176e100c863
Chris McDonnell <camcdonn@gmail.com>
2024-01-29 23:02:39 -0500

    chore: formatted file

--- lua/ecinori/lspconfig.lua
+++ lua/ecinori/lspconfig.lua
@@ -21,16 +21,16 @@ require('lspconfig').tsserver.setup({
     on_attach = on_attach,
     flags = lsp_flags,
     capabilities = capabilities,
-    filetypes = {"typescript", "typescriptreact", "typescript.tsx"},
-    cmd = {"typescript-language-server", "--stdio"}
+    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+    cmd = { "typescript-language-server", "--stdio" }
 })
 
 require('lspconfig').tailwindcss.setup({
     on_attach = on_attach,
     flags = lsp_flags,
     capabilities = capabilities,
-    filetypes = {"typescript", "typescriptreact", "typescript.tsx"},
-    cmd = {"tailwindcss-language-server", "--stdio"}
+    filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+    cmd = { "tailwindcss-language-server", "--stdio" }
 })
 
 require('lspconfig').lua_ls.setup({
@@ -71,37 +71,37 @@ vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 -- This is similiar to what I use
 local cmp = require("cmp")
 cmp.setup({
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "vsnip" },
-  },
-  snippet = {
-    expand = function(args)
-      -- Comes from vsnip
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  preselect = cmp.PreselectMode.None,
-  mapping = cmp.mapping.preset.insert({
-    -- None of this made sense to me when first looking into this since there
-    -- is no vim docs, but you can't have select = true here _unless_ you are
-    -- also using the snippet stuff. So keep in mind that if you remove
-    -- snippets you need to remove this select
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    -- I use tabs... some say you should stick to ins-completion but this is just here as an example
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
-  }),
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "vsnip" },
+    },
+    snippet = {
+        expand = function(args)
+            -- Comes from vsnip
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    preselect = cmp.PreselectMode.None,
+    mapping = cmp.mapping.preset.insert({
+        -- None of this made sense to me when first looking into this since there
+        -- is no vim docs, but you can't have select = true here _unless_ you are
+        -- also using the snippet stuff. So keep in mind that if you remove
+        -- snippets you need to remove this select
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        -- I use tabs... some say you should stick to ins-completion but this is just here as an example
+        ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ["<S-Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end,
+    }),
 })
