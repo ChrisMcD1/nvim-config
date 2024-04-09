0c7e56a455d4c7642687034e23c97d907903c283
Chris McDonnell <camcdonn@gmail.com>
2024-04-08 23:14:14 -0400

    feat(telescope): Shorten path display for deep scala files

--- lua/ecinori/telescope.lua
+++ lua/ecinori/telescope.lua
@@ -1,12 +1,17 @@
 require('telescope').setup {
+    defaults = {
+        path_display = {
+            shorten = { len = 1, exclude = { -1, -2, -3 } },
+        }
+    },
     pickers = {
         live_grep = {
             additional_args = function(opts)
-                return {'--hidden', '--iglob', '!.git'}
+                return { '--hidden', '--iglob', '!.git' }
             end
         },
         find_files = {
-            find_command = {'rg', '--files', '--iglob', '!.git', '--hidden' }
+            find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }
         }
     }
 }
