b7456eca22a3949f652a4b7bf262b7219c79f0d0
Chris McDonnell <camcdonn@gmail.com>
2024-12-09 23:14:35 -0500

    refactor(lazy): Replace config functions with opts

--- lua/plugins/gitlinker.lua
+++ lua/plugins/gitlinker.lua
@@ -2,8 +2,6 @@ return {
     {
         'ruifm/gitlinker.nvim',
         dependencies = { { 'nvim-lua/plenary.nvim' } },
-        config = function()
-            require('gitlinker').setup()
-        end
+        opts = {}
     }
 }
