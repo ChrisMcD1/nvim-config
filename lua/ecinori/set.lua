f3c8baa8f8b50b3b2c29b7a8e07c121b29c7756f
Chris McDonnell <camcdonn@gmail.com>
2024-12-11 23:54:16 -0500

    feat: Add a version of clipboard that might work on wsl and macos

--- lua/ecinori/set.lua
+++ lua/ecinori/set.lua
@@ -22,3 +22,18 @@ vim.opt.shortmess:append({ I = true })
 if vim.fn.executable('nvr') == 1 then
     vim.env.GIT_EDITOR = "nvr -cc split --remote-wait +'set bufhidden=wipe'"
 end
+
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
