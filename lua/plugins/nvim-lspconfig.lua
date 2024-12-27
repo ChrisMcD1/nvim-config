ee7ae23afb612b60bf480ad35ba34819daeeda2b
ChrisMcD1 <c.a.mcdonn@gmail.com>
2024-12-26 22:37:01 -0500

    refactor: Express autocmd as lua function

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -34,6 +34,11 @@ return {
             }
         })
 
-        vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function()
+                vim.lsp.buf.format()
+            end
+        })
     end
 }
