732e9e3cfd0f0d3141a345dae8d3a0d6bd9b8813
Chris McDonnell <c.a.mcdonn@gmail.com>
2025-03-30 19:43:54 -0400

    Adding border to hover

--- lua/ecinori/init.lua
+++ lua/ecinori/init.lua
@@ -5,3 +5,12 @@ require("ecinori.remap")
 require("ecinori.luasnip")
 
 vim.lsp.enable({"gopls", "lua_ls", "rust_analyzer", "tailwindcss", "ts_ls"})
+
+-- TODO: Some day I want to use winborder like this, but currently plenary
+-- generates double borders if vim.o.winborder is set, so I just set it on hover
+-- vim.o.winborder = "rounded"
+vim.keymap.set("n", "K", function()
+  vim.lsp.buf.hover({
+    border = "rounded",
+  })
+end)
