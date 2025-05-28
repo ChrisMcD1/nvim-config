4bb6a144123abeef243e1cdf698465b30101eb52
Chris McDonnell <c.a.mcdonn@gmail.com>
2025-05-28 10:04:47 -0400

    Add pyright

--- lua/ecinori/init.lua
+++ lua/ecinori/init.lua
@@ -4,13 +4,13 @@ require("ecinori.lazy")
 require("ecinori.remap")
 require("ecinori.luasnip")
 
-vim.lsp.enable({"gopls", "lua_ls", "rust_analyzer", "tailwindcss", "ts_ls"})
+vim.lsp.enable({ "gopls", "lua_ls", "rust_analyzer", "tailwindcss", "ts_ls", "pyright" })
 
 -- TODO: Some day I want to use winborder like this, but currently plenary
 -- generates double borders if vim.o.winborder is set, so I just set it on hover
 -- vim.o.winborder = "rounded"
 vim.keymap.set("n", "K", function()
-  vim.lsp.buf.hover({
-    border = "rounded",
-  })
+    vim.lsp.buf.hover({
+        border = "rounded",
+    })
 end)
